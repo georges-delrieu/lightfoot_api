81f10f223a75fbc5bc7c7d69420b64a0ddb732cf
georges delrieu <georges.delrieu@sciencespo.fr>
2021-04-25 11:44:30 +0200

    building database

---
+++ backend/app/db/migrations/versions/895c0bd033d5_create_main_tables.py
@@ -0,0 +1,33 @@
+"""create_main_tables
+
+Revision ID: 895c0bd033d5
+Revises: 
+Create Date: 2021-04-24 12:16:31.028671
+
+"""
+
+from alembic import op 
+import sqlalchemy as sa
+
+
+# revision identifiers, used by alembic
+revision = '895c0bd033d5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def create_footprint_table() -> None:
+    op.create_table(
+        "footprints",
+        sa.Column("id", sa.Integer, primary_key = True),
+        sa.Column("category", sa.Text, nullable = False),
+        sa.Column("subcategory", sa.Text, nullable = False, index = True),
+        sa.Column("item", sa.Text, nullable = False),
+        sa.Column("footprint", sa.Numeric(10,2), nullable = False),
+    )
+
+def upgrade() -> None:
+    pass
+
+def downgrades() -> None:
+    pass
